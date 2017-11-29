97e555b581914dc39842cdeaf03b92acf28dcc72
shy <shyamal.punekar@gmail.com>
2017-11-29 15:53:32 -0800

    show list of albums to user

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -11,11 +11,15 @@ class AlbumList extends Component {
     .then(response => this.setState({ albums: response.data }));
   }
 
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
+  }
+
   render() {
     console.log(this.state);
     return (
       <View>
-      <Text>Album List!</Text>
+      {this.renderAlbums()}
       </View>
     );
   }
